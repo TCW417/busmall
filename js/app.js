45c2d5b473223353335e6d397695a2031f9fa4d1
Tracy Williams <selpilot@gmail.com>
2018-04-16 14:47:02 -0700

    image cycling working

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
 // Globals
-var productID = 0;
-var prodArray = [];
+var productID = 0; // Give each product an internal ID
+var prodArray = []; // Array of product objects
+var voteCount = 25; // When === 0 show results
+var usedLastTurn = [999,999,999]; // ID's of last turn's pics
 
 // Constructor for Product object
 function Product(productName, imgFileName) {
@@ -24,8 +26,8 @@ new Product('Over-inflated Chair','chair.jpg');
 new Product('Toy Gargoyl','cthulhu.jpg');
 new Product('Duck\'s Beak Muzzle','dog-duck.jpg');
 new Product('Canned Dragon Meat','dragon.jpg');
-new Product('Bic Pen Cap Cutlery','pen-left.jpg');
-new Product('Pen Floor Sweeping Footies','pet-sweep.jpg');
+new Product('Bic Pen Cap Cutlery','pen-left.png');
+new Product('Floor Sweeping Pet Footies','pet-sweep.jpg');
 new Product('Pizza Cutting Scissors','scissors.jpg');
 new Product('Kid\'s Shark Sleeping Bag','shark.jpg');
 new Product('Baby Sweeping Onesie','sweep.png');
@@ -34,3 +36,45 @@ new Product('Canned Unicorn Meat','unicorn.jpg');
 new Product('Wiggling USB Dragon Tail','usb.gif');
 new Product('Recycling Watering Can','water-can.jpg');
 new Product('Boquet-Retaining Wine Glass','wine-glass.jpg');
+
+function getThreeRandomImageIndices() {
+  // return three random product indexes that aren't in
+  // usedLastTurn array
+  var i = 0;
+  var usedThisTurn = [];
+  while (i < 3) {
+    var r = Math.floor(Math.random() * prodArray.length);
+    if (!usedLastTurn.includes(r) && !usedThisTurn.includes(r)) { // r is unique last turn
+      usedThisTurn.push(r);
+      i++;
+    }
+  }
+  console.log('last turn',usedLastTurn,'this turn',usedThisTurn);
+  usedLastTurn = usedThisTurn;
+  return usedThisTurn;
+}
+
+// voteCount = 4;
+// while (voteCount > 0) {
+function displayThreeImages() {
+  // Select three images at random that weren't used last turn
+  var displayList = getThreeRandomImageIndices();
+  // Display the three images on the page
+  for (var img = 0; img < displayList.length; img++) {
+    // modify DOM for image to display file and name
+    var imgEl = document.getElementById(
+      'i'+img);
+    imgEl.src = prodArray[displayList[img]].src;
+    var capEl = document.getElementById('c'+img);
+    capEl.textContent = prodArray[displayList[img]].name;
+  }
+  // voteCount--;
+}
+
+displayThreeImages();
+
+var figure0 = document.getElementById('figure0');
+figure0.addEventListener('click',function(e){
+  console.log('clicked');
+  displayThreeImages();
+});
